5f67f5963696719311c2903dfeb7954789dc1b76
harrykao <knight65210@gmail.com>
2023-12-01 15:25:26 +0800

    modify app.py

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.get("/hello")
 def index():
-    return "Welcome to my Flask App"
+    return "Hello world"
 
+@app.get("/")
+def index():
+    return "welcome to flask"
 
 # if __name__ == "__main__":
 #     # Dev only: run "python main.py" and open http://localhost:8080
