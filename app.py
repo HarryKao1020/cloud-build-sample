6d96f255485cb9ef8bb4069368664690c557a369
harrykao <knight65210@gmail.com>
2023-12-06 11:56:24 +0800

    modify port

--- app.py
+++ app.py
@@ -65,8 +65,10 @@ def handle_message(event):
         TextSendMessage(text=reply_text)
     )
 
+port = int(os.environ.get('PORT', 8000))    
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=port)
 
 
 
